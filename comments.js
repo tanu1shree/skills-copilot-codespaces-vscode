a8a323c3065b48111c853ed07801ccc57724a891
tanushree <191781128+tanu1shree@users.noreply.github.com>
2026-04-30 18:22:13 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,10 @@
+// Create web server
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+server.listen(3000, '
